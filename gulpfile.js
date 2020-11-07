43e7278d29df9e3d1f3cd529316037db307c1e47
props98 <props98@yahoo.com>
2020-11-07 16:56:02 +0300

    Добавлена пвтоматизация, Gulp сборка, подключение

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,35 @@
-// Подключаем Gulp
-const { parallel } = require("gulp");
+// "use strict";
+
 var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 
-// // Создаем простой таск
-// gulp.task('myFirstTask', function (done) {
-//   console.log('Привет, я твой первый таск!');
-//   done()
-// });
-
-// // Запуск тасков по умолчанию
-// gulp.task("default", gulp.series('myFirstTask'));
-
-// Подключаем плагины Gulp
-var sass = require("gulp-sass"), // переводит SASS в CSS
-  cssnano = require("gulp-cssnano"), // Минимизация CSS
-  autoprefixer = require('gulp-autoprefixer'), // Проставлет вендорные префиксы в CSS для поддержки старых браузеров
-  imagemin = require('gulp-imagemin'), // Сжатие изображение
-  concat = require("gulp-concat"), // Объединение файлов - конкатенация
-  uglify = require("gulp-uglify"), // Минимизация javascript
-  rename = require("gulp-rename"); // Переименование файлов
-
-// Tasks /////
-
-// Копирование файлов HTML в папку dist
-gulp.task("html", function(done) {
-  return gulp.src("source/*.html")
-    .pipe(gulp.dest("dist"));
-    done();
-});
-
-// Объединение, компиляция Sass в CSS, простановка венд. префиксов и дальнейшая минимизация кода
-gulp.task("sass", function(done) {
-  return gulp.src("source/sass/*.sass")
-    .pipe(concat('styles.sass'))
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.sass")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cssnano())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest("dist/css"));
-    done();
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
 
-// Объединение и сжатие JS-файлов
-gulp.task("scripts", function(done) {
-  return gulp.src("source/js/*.js") // директория откуда брать исходники
-    .pipe(concat('scripts.js')) // объеденим все js-файлы в один 
-    .pipe(uglify()) // вызов плагина uglify - сжатие кода
-    .pipe(rename({ suffix: '.min' })) // вызов плагина rename - переименование файла с приставкой .min
-    .pipe(gulp.dest("dist/js")); // директория продакшена, т.е. куда сложить готовый файл
-    done();
+gulp.task("serve", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
-// Сжимаем картинки
-gulp.task('img', function(done) {
-  return gulp.src("source/images/*.+(jpg|jpeg|png|gif)")
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      interlaced: true
-    }))
-    .pipe(gulp.dest("dist/images"));
-    done();
-});
-
-// Задача слежения за измененными файлами
-gulp.task("watch", function(done) {
-  gulp.watch("source/*.html", ["html"]);
-  gulp.watch("source/js/*.js", ["scripts"]);
-  gulp.watch("source/sass/*.sass", ["sass"]);
-  gulp.watch("source/images/*.+(jpg|jpeg|png|gif)", ["img"]);
-  done();
-});
-
-///// Таски ///////////////////////////////////////
-
-// Запуск тасков по умолчанию
-gulp.task("default", gulp.series('html', 'sass', 'scripts', 'img', 'watch'));
+// gulp.task("start", gulp.series("css", "serve"));
